aa1accfb6b2109b284f70e46651dd9290a0a453b
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2024-12-21 22:21:13 +0100

    Implement Circuit Breaker

--- src/main/java/org/example/garagemanagementgatewayservice/config/GatewayConfig.java
+++ src/main/java/org/example/garagemanagementgatewayservice/config/GatewayConfig.java
@@ -1,25 +1,60 @@
 package org.example.garagemanagementgatewayservice.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Getter
+@Setter
+@RefreshScope
 public class GatewayConfig {
 
+    @Value("${client-service-url:http://localhost:8081}")
+    private String clientServiceUrl;
+
+    @Value("${vehicule-service-url:http://localhost:8082}")
+    private String vehiculeServiceUrl;
+
+    @Value("${workshop-service-url:http://localhost:8083}")
+    private String workshopServiceUrl;
+
+    @Value("${invoice-service-url:http://localhost:8084}")
+    private String invoiceServiceUrl;
+
+    @Value("${notification-service-url:http://localhost:8085}")
+    private String notificationServiceUrl;
+
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator customRoutes(RouteLocatorBuilder builder) {
+
+
         return builder.routes()
-                // Workshop Service Route
-                .route("workshop-route", r -> r.path("/api/v1/workshop/**")
-                        .filters(f -> f.addRequestHeader("X-Powered-By", "BTBG Gateway Service"))
-                        .uri("http://localhost:8083")) // Update to match your service URL
-                // Notification Service Route
-                .route("notification-route", r -> r.path("/api/v1/notification/**")
-                        .filters(f -> f.addRequestHeader("X-Powered-By", "BTBG Gateway Service"))
-                        .uri("http://localhost:8084")) // Update to match your service URL
+                .route("client-service-route", r -> r.path("/api/v1/clients/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("clientCircuitBreaker")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(clientServiceUrl))
+                .route("vehicule-service-route", r -> r.path("/api/v1/vehicules/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("vehiculeCircuitBreaker")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(vehiculeServiceUrl))
+                .route("workshop-service-route", r -> r.path("/api/v1/workshop/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("workshopCircuitBreaker")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(workshopServiceUrl))
+                .route("invoice-service-route", r -> r.path("/api/v1/invoices/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("invoiceCircuitBreaker")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(invoiceServiceUrl))
+                .route("notification-service-route", r -> r.path("/api/v1/notification/**")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("notificationCircuitBreaker")
+                                .setFallbackUri("forward:/fallback")))
+                        .uri(notificationServiceUrl))
                 .build();
     }
 }
-
